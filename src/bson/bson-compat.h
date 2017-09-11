08de62060df67b236f1fece4a5fe1fbb4392b83e
Erik Olofsson <erik.olofsson@hansoft.com>
2017-09-11 22:26:10 +0200

    Fix MSVC compile errors and warnings

--- src/bson/bson-compat.h
+++ src/bson/bson-compat.h
@@ -39,6 +39,7 @@
 
 
 #ifdef BSON_OS_WIN32
+#include "sdkddkver.h"
 #if defined(_WIN32_WINNT) && (_WIN32_WINNT < 0x0600)
 #undef _WIN32_WINNT
 #endif
@@ -87,7 +88,9 @@ BSON_BEGIN_DECLS
 
 #ifdef _MSC_VER
 #include <time.h>
+#include <inttypes.h>
 #include "bson-stdint-win32.h"
+
 #ifndef __cplusplus
 /* benign redefinition of type */
 #pragma warning(disable : 4142)
